cccd735d2a8d76c52581e8bc9e56472d9917d6e2
babaev2023 <stepan.babaev@gmail.com>
2023-07-06 15:16:18 +0300

    NextAPI

--- GO_lvl2/README.md
+++ GO_lvl2/README.md
@@ -6,6 +6,8 @@
 Lec1 JSON Сериализация и Десериализация  
 Lec2 JSON Когда нет понимания что за файл  
 Lec3 REST API  
+Lec4 REST API-пример  
+Lec5 REST API-пример получше  
 
 
   
---
+++ GO_lvl2/lec5/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"lec5/utils"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+const (
+	apiPrefix string = "/api/v1"
+)
+
+var (
+	port                    string
+	bookResourcePrefix      string = apiPrefix + "/book"  //api/v1/book/
+	manyBooksResourcePrefix string = apiPrefix + "/books" //api/v1/books/
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("could not find .env file:", err)
+	}
+	port = os.Getenv("app_port")
+}
+
+func main() {
+
+	log.Println("Starting REST API server on port:", port)
+	//log.Fatal(http.ListenAndServe(":"+port, nil))
+	router := mux.NewRouter()
+
+	utils.BuildBookResource(router, bookResourcePrefix)
+	utils.BuildManyBooksResourcePrefix(router, manyBooksResourcePrefix)
+
+	log.Println("Router initalizing successfully. Ready to go!")
+	log.Fatal(http.ListenAndServe(":"+port, router))
+
+}
