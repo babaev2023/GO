cae462914d4fdb47b94bec58e3dd13b805839948
babaev2023 <stepan.babaev@gmail.com>
2023-07-06 16:35:10 +0300

    path toml

--- GO_lvl2/lec6/cmd/api/main.go
+++ GO_lvl2/lec6/cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/BurntSushi/toml"
@@ -8,14 +9,20 @@ import (
 )
 
 var (
-	configPath string = "configs/api.toml"
+	configPath string
+	// string = "configs/api.toml"
 )
 
 func init() {
+	//Скажем, что наше приложение будет на этапе запуска получать путь до конфиг файла из внешнего мира
+	flag.StringVar(&configPath, "path", "configs/api.toml", "path to config file in .toml format")
 
 }
 
 func main() {
+	//В этот момент происходит инициализация переменной configPath значением
+	flag.Parse()
+
 	log.Println("Works")
 	//server instance initialization
 	config := api.NewConfig()
